23e25f55a9842018c3233e7ddcce324dff0099b0
azima786 <azima786@gmail.com>
2021-05-15 23:52:30 -0500

    testbranch1

---
+++ src/main/java/TestNG/gittest1.java
@@ -0,0 +1,12 @@
+package TestNG;
+
+public class gittest1 {
+
+	public static void main(String[] args) {
+printMe();
+printMe1();
+
+	}
+	public static void printMe() {System.out.println("git test");}
+	public static void printMe1() {System.out.println("git test 1");}
+}
